47cd8a4c589b0c6b3c7c52eba5577d945ef76248
Jagdeep gill [jatt life thuglife] <64634071+gillzxo@users.noreply.github.com>
2022-11-19 01:23:21 -0800

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Jane Doe',
     title: "Web Developer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: 'https://github.com/gillzxo/Dpo/blob/eab3e5dfcc1a9220d20430fd3a2c8b964940f93b/Untitled%20design%20(4).png',
     resumePdf: resume
 }
