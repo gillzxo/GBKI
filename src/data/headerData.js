f748a1bd85c1ff8927d00e85a2c21ab23bbc3dbe
Jagdeep gill [jatt life thuglife] <64634071+gillzxo@users.noreply.github.com>
2022-11-19 01:26:48 -0800

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
 import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
-    name: 'Jane Doe',
-    title: "Web Developer",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+    name: '1ST CHOICE',
+    title: "Education Consultant",
+    desciption:"IELTS PTE and Immigration Services 🇨🇦🇦🇺🇺🇲🇬🇧 STUDY/ VISITOR/ SOWP/ INVESTOR/PR ",
     image: 'https://github.com/gillzxo/Dpo/blob/eab3e5dfcc1a9220d20430fd3a2c8b964940f93b/Untitled%20design%20(4).png',
     resumePdf: resume
 }
